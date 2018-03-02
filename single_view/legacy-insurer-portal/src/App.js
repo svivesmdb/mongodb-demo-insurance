af3da7079ee388821830081b67c56285deddb703
mentlsve <sven.mentl@gmail.com>
2018-03-02 15:08:38 +0100

    form for creating car insurance

--- single_view/legacy-insurer-portal/src/App.js
+++ single_view/legacy-insurer-portal/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NewCarInsurancePolicy from './components/NewCarInsurancePolicy';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+      <div className="applicationHeader">
+          <h1>Insurer Portal v1.0</h1>
+          </div>
+        <NewCarInsurancePolicy />
       </div>
     );
   }
