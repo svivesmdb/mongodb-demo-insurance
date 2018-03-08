1c04afaa2fbf9e2f963fe1165c7bb0ec31ad615e
mentlsve <sven.mentl@gmail.com>
2018-03-08 10:05:59 +0100

    navigation component

---
+++ single_view/legacy-insurer-portal/src/components/Navigation.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
+
+export default class Navigation extends Component {
+
+    render() {
+        return (
+            <div className="containerBorder">
+                <nav>
+                    <Link to="/viewcustomers">View Customers</Link>
+                    <Link to="/createcustomer">Create Customer</Link>
+                    <Link to="/createcarpolicy">Create Car Insurance Policy</Link>
+                    <Link to="/createhomepolicy">Creat Home Insurance Policy</Link>
+                    <Link to="/exit">Exit</Link>
+                </nav>
+            </div>
+        )
+    }
+}
